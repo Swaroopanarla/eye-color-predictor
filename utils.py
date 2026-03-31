25d2d166944a0831eb26d2cfe5a25fd4e4664108
Swaroopanarla <113962405+Swaroopanarla@users.noreply.github.com>
2026-03-31 16:49:43 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data():
-    data = pd.read_csv("eye_dataset.csv")
-    return data
-
-def get_info_by_eye_color(data, eye_color):
-    result = data[data["eye_color"].str.lower() == eye_color.lower()]
-    return result
